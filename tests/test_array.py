8ecf58297464ac951d0bb57454d9c09b9b66395e
domna <florian.dobener@physik.hu-berlin.de>
2023-04-23 20:54:03 +0200

    Adds python tests

---
+++ tests/test_array.py
@@ -0,0 +1,17 @@
+"""Some basic python tests"""
+import pytest
+import numpy as np
+from numpy.testing import assert_array_almost_equal
+from formula_parser import parse_formula_dispersion
+
+def test_array_parsing():
+    """Array parsed properly"""
+
+    parsed = parse_formula_dispersion("3 * 3 * lbda", "lbda", np.array([1., 2., 3.]))
+    assert_array_almost_equal(parsed, np.array([9., 18., 27.]))
+
+def test_fails_on_wrong_token():
+    """Array parsing fails on wrong token"""
+
+    with pytest.raises(TypeError):
+        parse_formula_dispersion("3 * 3 * lba", "lbda", np.array([1., 2., 3.]))
