db8042e0a6ff99f11cf15d4165923f795e69fb73
domna <florian.dobener@physik.hu-berlin.de>
2023-04-23 12:50:10 +0200

    Rearrangements

--- src/lib.rs
+++ src/lib.rs
@@ -12,12 +12,15 @@ lalrpop_mod!(formula_dispersion);
 
 #[test]
 fn basic_execution_test() {
-    use numpy::ndarray::aview1;
+    use numpy::ndarray::{aview1, aview2};
 
     let x_axis_name = "lbda";
     let a1 = aview1(&[0., 1., 2., 3.]);
+    let a2 = aview2(&[[0., 1., 2.], [4., 5., 6.]]);
+    let size = a2.len();
     assert!(formula_dispersion::ExprParser::new().parse(x_axis_name, &a1, "22").is_ok());
     assert!(formula_dispersion::ExprParser::new().parse(x_axis_name, &a1, "(22)").is_ok());
+    assert!(formula_dispersion::ExprParser::new().parse(x_axis_name, &a2.into_shape([size]).unwrap(), "(22)").is_ok());
     assert!(
         formula_dispersion::ExprParser::new()
             .parse(x_axis_name, &a1, "2 * 10")
@@ -32,24 +35,24 @@ fn basic_execution_test() {
         .is_err());
 }
 
+fn parse_formula_dispersion<T>(formula: &str, x_axis_name: &str, x_axis_values: ArrayViewD<'_, T>) -> PyResult<String> {
+    let size = x_axis_values.len();
+    match x_axis_values.into_shape([size]) {
+        Ok(x_axis) => {
+            let parsed =
+                formula_dispersion::ExprParser::new().parse(&x_axis_name, &x_axis, &formula);
+            
+            match parsed {
+                Ok(v) => return Ok(v.to_string()),
+                Err(err) => return Err(PyErr::new::<PyTypeError, _>(err.to_string())),
+            }
+        },
+        Err(err) => return Err(PyErr::new::<PyTypeError, _>(err.to_string()))
+    }
+}
+
 #[pymodule]
 fn formula_parser(_py: Python, m: &PyModule) -> PyResult<()> {
-
-    fn parse_formula_dispersion<T>(formula: &str, x_axis_name: &str, x_axis_values: ArrayViewD<'_, T>) -> PyResult<String> {
-        let size = x_axis_values.len();
-        match x_axis_values.into_shape([size]) {
-            Ok(x_axis) => {
-                let parsed =
-                    formula_dispersion::ExprParser::new().parse(&x_axis_name, &x_axis, &formula);
-                
-                match parsed {
-                    Ok(v) => return Ok(v.to_string()),
-                    Err(err) => return Err(PyErr::new::<PyTypeError, _>(err.to_string())),
-                }
-            },
-            Err(err) => return Err(PyErr::new::<PyTypeError, _>(err.to_string()))
-        }
-    }
 
     #[pyfn(m)]
     #[pyo3(name = "parse_formula_dispersion")]
