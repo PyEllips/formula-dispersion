62bc0e5287cbb070d6f868f956d9d7a6a2f4fcd7
domna <florian.dobener@physik.hu-berlin.de>
2023-04-23 20:53:56 +0200

    Work with complex array

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
-use numpy::ndarray::ArrayViewD;
-use numpy::PyReadonlyArrayDyn;
-use pyo3::{pymodule, types::PyModule, PyResult, Python};
-// use pyo3::types::PyDict;
+use std::error;
 use pyo3::prelude::*;
+use pyo3::{pymodule, types::PyModule, PyResult, Python}; 
 use pyo3::exceptions::PyTypeError;
+use numpy::{PyReadonlyArrayDyn, PyArray1, IntoPyArray};
+use numpy::ndarray::{Array1, ArrayViewD};
+use num_complex::Complex64;
 
 #[macro_use]
 extern crate lalrpop_util;
@@ -25,7 +26,12 @@ fn basic_execution_test() {
         formula_dispersion::ExprParser::new()
             .parse(x_axis_name, &a1, "2 * 10")
             .unwrap()
-            == 20.
+            == aview1(&[
+                Complex64::new(20., 0.),
+                Complex64::new(20., 0.),
+                Complex64::new(20., 0.),
+                Complex64::new(20., 0.)
+            ])
     );
     assert!(formula_dispersion::ExprParser::new()
         .parse(x_axis_name, &a1, "((((22))))")
@@ -35,20 +41,15 @@ fn basic_execution_test() {
         .is_err());
 }
 
-fn parse_formula_dispersion<T>(formula: &str, x_axis_name: &str, x_axis_values: ArrayViewD<'_, T>) -> PyResult<String> {
+fn parse_formula_dispersion<'a>(
+    formula: &'a str,
+    x_axis_name: &'a str,
+    x_axis_values: ArrayViewD<'a, f64>
+) -> Result<Array1<Complex64>, Box<dyn error::Error + 'a>> {
     let size = x_axis_values.len();
-    match x_axis_values.into_shape([size]) {
-        Ok(x_axis) => {
-            let parsed =
-                formula_dispersion::ExprParser::new().parse(&x_axis_name, &x_axis, &formula);
-            
-            match parsed {
-                Ok(v) => return Ok(v.to_string()),
-                Err(err) => return Err(PyErr::new::<PyTypeError, _>(err.to_string())),
-            }
-        },
-        Err(err) => return Err(PyErr::new::<PyTypeError, _>(err.to_string()))
-    }
+    let x_axis_1d = x_axis_values.into_shape([size])?;
+    
+    Ok(formula_dispersion::ExprParser::new().parse(&x_axis_name, &x_axis_1d, &formula)?)
 }
 
 #[pymodule]
@@ -57,14 +58,18 @@ fn formula_parser(_py: Python, m: &PyModule) -> PyResult<()> {
     #[pyfn(m)]
     #[pyo3(name = "parse_formula_dispersion")]
     fn parse_formula_dispersion_py<'py>(
+        py: Python<'py>,
         formula: &str,
         x_axis_name: &str,
         x_axis_values: PyReadonlyArrayDyn<f64>,
         // single_params: &PyDict,
         // rep_params: &PyDict
-    ) -> PyResult<String> {
+    ) -> PyResult<&'py PyArray1<Complex64>> {
         let x: numpy::ndarray::ArrayBase<numpy::ndarray::ViewRepr<&f64>, numpy::ndarray::Dim<numpy::ndarray::IxDynImpl>> = x_axis_values.as_array();
-        return parse_formula_dispersion(formula, x_axis_name, x);
+        match parse_formula_dispersion(formula, x_axis_name, x) {
+            Ok(arr) => return Ok(arr.into_pyarray(py)),
+            Err(err) => return Err(PyErr::new::<PyTypeError, _>(err.to_string()))
+        }
     }
 
     Ok(())
