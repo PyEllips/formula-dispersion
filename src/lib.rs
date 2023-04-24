62c306a7d1d8993edd96b2557c9553554aa83d1a
domna <florian.dobener@physik.hu-berlin.de>
2023-04-24 08:13:05 +0200

    Autoformatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
-use std::error;
-use pyo3::prelude::*;
-use pyo3::{pymodule, types::PyModule, PyResult, Python}; 
-use pyo3::exceptions::PyTypeError;
-use numpy::{PyReadonlyArrayDyn, PyArray1, IntoPyArray};
-use numpy::ndarray::{Array1, ArrayViewD};
 use num_complex::Complex64;
+use numpy::ndarray::{Array1, ArrayViewD};
+use numpy::{IntoPyArray, PyArray1, PyReadonlyArrayDyn};
+use pyo3::exceptions::PyTypeError;
+use pyo3::prelude::*;
+use pyo3::{pymodule, types::PyModule, PyResult, Python};
+use std::error;
 
 #[macro_use]
 extern crate lalrpop_util;
@@ -19,9 +19,15 @@ fn basic_execution_test() {
     let a1 = aview1(&[0., 1., 2., 3.]);
     let a2 = aview2(&[[0., 1., 2.], [4., 5., 6.]]);
     let size = a2.len();
-    assert!(formula_dispersion::ExprParser::new().parse(x_axis_name, &a1, "22").is_ok());
-    assert!(formula_dispersion::ExprParser::new().parse(x_axis_name, &a1, "(22)").is_ok());
-    assert!(formula_dispersion::ExprParser::new().parse(x_axis_name, &a2.into_shape([size]).unwrap(), "(22)").is_ok());
+    assert!(formula_dispersion::ExprParser::new()
+        .parse(x_axis_name, &a1, "22")
+        .is_ok());
+    assert!(formula_dispersion::ExprParser::new()
+        .parse(x_axis_name, &a1, "(22)")
+        .is_ok());
+    assert!(formula_dispersion::ExprParser::new()
+        .parse(x_axis_name, &a2.into_shape([size]).unwrap(), "(22)")
+        .is_ok());
     assert!(
         formula_dispersion::ExprParser::new()
             .parse(x_axis_name, &a1, "2 * 10")
@@ -30,7 +36,7 @@ fn basic_execution_test() {
                 Complex64::new(20., 0.),
                 Complex64::new(20., 0.),
                 Complex64::new(20., 0.),
-                Complex64::new(20., 0.)
+                Complex64::new(20., 0.),
             ])
     );
     assert!(formula_dispersion::ExprParser::new()
@@ -44,17 +50,16 @@ fn basic_execution_test() {
 fn parse_formula_dispersion<'a>(
     formula: &'a str,
     x_axis_name: &'a str,
-    x_axis_values: ArrayViewD<'a, f64>
+    x_axis_values: ArrayViewD<'a, f64>,
 ) -> Result<Array1<Complex64>, Box<dyn error::Error + 'a>> {
     let size = x_axis_values.len();
     let x_axis_1d = x_axis_values.into_shape([size])?;
-    
+
     Ok(formula_dispersion::ExprParser::new().parse(&x_axis_name, &x_axis_1d, &formula)?)
 }
 
 #[pymodule]
 fn formula_parser(_py: Python, m: &PyModule) -> PyResult<()> {
-
     #[pyfn(m)]
     #[pyo3(name = "parse_formula_dispersion")]
     fn parse_formula_dispersion_py<'py>(
@@ -65,10 +70,13 @@ fn formula_parser(_py: Python, m: &PyModule) -> PyResult<()> {
         // single_params: &PyDict,
         // rep_params: &PyDict
     ) -> PyResult<&'py PyArray1<Complex64>> {
-        let x: numpy::ndarray::ArrayBase<numpy::ndarray::ViewRepr<&f64>, numpy::ndarray::Dim<numpy::ndarray::IxDynImpl>> = x_axis_values.as_array();
+        let x: numpy::ndarray::ArrayBase<
+            numpy::ndarray::ViewRepr<&f64>,
+            numpy::ndarray::Dim<numpy::ndarray::IxDynImpl>,
+        > = x_axis_values.as_array();
         match parse_formula_dispersion(formula, x_axis_name, x) {
             Ok(arr) => return Ok(arr.into_pyarray(py)),
-            Err(err) => return Err(PyErr::new::<PyTypeError, _>(err.to_string()))
+            Err(err) => return Err(PyErr::new::<PyTypeError, _>(err.to_string())),
         }
     }
 
--- tests/test_array.py
+++ tests/test_array.py
@@ -4,14 +4,16 @@ import numpy as np
 from numpy.testing import assert_array_almost_equal
 from formula_parser import parse_formula_dispersion
 
+
 def test_array_parsing():
     """Array parsing works properly"""
 
-    parsed = parse_formula_dispersion("3 * 3 * lbda", "lbda", np.array([1., 2., 3.]))
-    assert_array_almost_equal(parsed, np.array([9., 18., 27.]))
+    parsed = parse_formula_dispersion("3 * 3 * lbda", "lbda", np.array([1.0, 2.0, 3.0]))
+    assert_array_almost_equal(parsed, np.array([9.0, 18.0, 27.0]))
+
 
 def test_fails_on_wrong_token():
     """Array parsing fails on wrong token"""
 
     with pytest.raises(TypeError):
-        parse_formula_dispersion("3 * 3 * lba", "lbda", np.array([1., 2., 3.]))
+        parse_formula_dispersion("3 * 3 * lba", "lbda", np.array([1.0, 2.0, 3.0]))
